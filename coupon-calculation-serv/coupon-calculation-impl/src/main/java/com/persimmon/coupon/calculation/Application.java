14215a7a4d8c3ea6640635a1f504483cf576b3ca
persimmon <819910532@qq.com>
2022-09-27 14:11:40 +0800

    实现服务注册

---
+++ coupon-calculation-serv/coupon-calculation-impl/src/main/java/com/persimmon/coupon/calculation/Application.java
@@ -0,0 +1,16 @@
+package com.persimmon.coupon.calculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.persimmon"})
+@EnableDiscoveryClient
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
